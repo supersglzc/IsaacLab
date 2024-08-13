1bc842131292baa9fad78b3a7e777cafd312c2cc
Steven Li <zc.li@dexmate.ai>
2024-08-13 16:04:33 -0700

    remove reward dt

--- source/extensions/omni.isaac.lab/omni/isaac/lab/managers/reward_manager.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/managers/reward_manager.py
@@ -136,7 +136,7 @@ class RewardManager(ManagerBase):
             if term_cfg.weight == 0.0:
                 continue
             # compute term's value
-            value = term_cfg.func(self._env, **term_cfg.params) * term_cfg.weight * dt
+            value = term_cfg.func(self._env, **term_cfg.params) * term_cfg.weight # * dt
             # update total reward
             self._reward_buf += value
             # update episodic sum
